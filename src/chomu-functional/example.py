99d07731c60db7b3f976515e98f0e0a156902007
ChanHaeng Lee <2chanhaeng@gmail.com>
2024-02-11 23:42:38 +0900

    Create functions

---
+++ src/chomu-functional/example.py
@@ -0,0 +1,18 @@
+def add(a):
+    return lambda b: b + a
+
+
+def sub(a):
+    return lambda b: b - a
+
+
+def mul(a):
+    return lambda b: b * a
+
+
+def div(a):
+    return lambda b: b / a
+
+
+def mod(a):
+    return lambda b: b % a
